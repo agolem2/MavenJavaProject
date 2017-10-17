6092f2f32ee06c10de4fa6adde25f97fdad431fd
Milo Onken <eronken@gmail.com>
2017-10-17 10:16:30 -0700

    Adding Coding Challenge Test Suite

---
+++ src/CodingChallenge/AllCodingChallengeTests.java
@@ -0,0 +1,13 @@
+package CodingChallenge;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CodingChallenge01.class, CodingChallenge02.class, CodingChallenge03.class, CodingChallenge04.class,
+		CodingChallenge05.class, CodingChallenge06.class, CodingChallenge07.class, CodingChallenge08.class,
+		CodingChallenge09.class, CodingChallenge10.class, CodingChallenge11.class })
+public class AllCodingChallengeTests {
+
+}
