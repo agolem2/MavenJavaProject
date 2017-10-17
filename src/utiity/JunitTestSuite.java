57322e7d417eb28234cd16673d88f7b90becaee5
Milo Onken <eronken@gmail.com>
2017-10-17 10:19:35 -0700

    Adding JUnit Test Suite

--- src/utiity/JunitTestSuite.java
+++ src/utiity/JunitTestSuite.java
@@ -1,14 +1,24 @@
 package utiity;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
 
-import Test.bhhctest;
+import CodingChallenge.CodingChallenge01;
+import CodingChallenge.CodingChallenge02;
+import CodingChallenge.CodingChallenge03;
+import CodingChallenge.CodingChallenge04;
+import CodingChallenge.CodingChallenge05;
+import CodingChallenge.CodingChallenge06;
+import CodingChallenge.CodingChallenge07;
+import CodingChallenge.CodingChallenge08;
+import CodingChallenge.CodingChallenge09;
+import CodingChallenge.CodingChallenge10;
+import CodingChallenge.CodingChallenge11;
 
 @RunWith(Suite.class)
-
-@Suite.SuiteClasses({
-   bhhctest.class
-})
+@SuiteClasses({ CodingChallenge01.class, CodingChallenge02.class, CodingChallenge03.class, CodingChallenge04.class,
+		CodingChallenge05.class, CodingChallenge06.class, CodingChallenge07.class, CodingChallenge08.class,
+		CodingChallenge09.class, CodingChallenge10.class, CodingChallenge11.class })
 
 public class JunitTestSuite {   
 }  	
